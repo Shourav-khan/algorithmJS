63c54ab0da3eb6f491ca7e0e119a50727fb8438a
Shourav Khan <102287391+Shourav-khan@users.noreply.github.com>
2022-09-09 12:50:27 +0600

    string counting

---
+++ DtaStructure/practise.js
@@ -0,0 +1,50 @@
+
+// function getHigherLowerValue(arr){
+
+//     let higher = arr[0];
+//     let lower = arr[0];
+
+//     for(let i=0; i<arr.length; i++){
+
+//         if(higher < arr[i]){
+
+//             higher = arr[i];
+
+//         }
+
+//         if(lower > arr[i]){
+
+//             lower =arr[i];
+
+//         }
+
+//     }
+//     console.log(higher,lower);
+
+// }
+
+
+// getHigherLowerValue([9, -9, 2, 3, -5])
+
+
+
+// function charCount(str){
+
+//     let allChar={}
+
+//     for(let i=0; i<str.length; i++){
+
+//         let char = str[i];
+//         if(allChar[char]>0){
+
+//             allChar[char]++;
+
+//         }else{
+//             allChar[char] = 1;
+//         }
+
+//     }
+// }
+
+
+// charCount("Hello Everyone");
