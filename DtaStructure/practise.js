467d951b701e99ca99600af33135c1b724db7eb2
Shourav Khan <102287391+Shourav-khan@users.noreply.github.com>
2022-09-19 17:07:01 +0600

    lifo

--- DtaStructure/practise.js
+++ DtaStructure/practise.js
@@ -222,9 +222,89 @@
 
 // }
 
-// console.log(bubbleSort([1,8,4,9,-2,4]))
+// console.log(bubbleSort([8,1,4,9,-2,4]))
 
 
 ///////////////////////////////////////////////////////////
 
+
+// Selection Sort
+
+//////////////////////////////////////////////////////////
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// var twoSum = function(nums, target) {
+
+
+//     for(let i= 0; i<nums.length; i++){
+//         for(let j= i+1; j<nums.length; j++){
+
+//             if(nums[i]+nums[j] === target) return [i,j]
+
+//         }
+//     }
+
+//     return false
+    
+// };
+
+// console.log(twoSum([3,2,4],6))
+
+/////////////////////////////////////////////////////////
+
+// Array value insert 
+
+// class arrayValue{ 
+
+//     constructor (){
+
+//         this.storage = [];
+        
+//     }
+
+//     insert(value){
+
+//         this.storage.push(value);
+
+//         return this.storage;
+
+//     }
+// };
+
+// let av = new arrayValue();
+
+// console.log(av.insert(20));
+
+//////////////////////////////////////////
+
+// Stack(Last In First Out)
+
+/////////////////////////////////////////
+
+class Stack{
+
+    constructor(){
+        this.arr = [];
+
+    }
+        added(value){
+
+            this.arr.push(value);
+            return this.arr;
+    }
+
+    berKora(){
+        this.arr.pop();
+        return this.arr;
+    }
+}
+
+let stack = new Stack();
+
+console.log(stack.added(20))
+console.log(stack.added(200))
+console.log(stack.berKora())
 
