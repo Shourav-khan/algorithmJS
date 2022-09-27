9dfc2729e85fdf517419d924cd135d61191d1ed5
Shourav Khan <khanraveen321@gmail.com>
2022-09-28 01:22:05 +0600

    call back function

--- DtaStructure/javaScript.js
+++ DtaStructure/javaScript.js
@@ -45,31 +45,78 @@
 // Male or female detect
 /////////////////////////////////////
 
-function person(first_name,last_name, gender){
+// function person(first_name,last_name, gender){
 
-    var output;
-    var output1;
+//     var output;
+//     var output1;
 
-        if(gender === 'male'){
+//         if(gender === 'male'){
 
-             output = 'Mr.' +first_name+ ' ' +last_name;
+//              output = 'Mr.' +first_name+ ' ' +last_name;
 
            
 
-        }else if(gender === 'female')
-        {
-             output1 = 'Mrs.' +first_name+ ' ' +last_name;
+//         }else if(gender === 'female')
+//         {
+//              output1 = 'Mrs.' +first_name+ ' ' +last_name;
              
-             return output1;
+//              return output1;
 
-        }else{
+//         }else{
 
-            return 'kew na'
+//             return 'kew na'
 
-        }
-        return output;
+//         }
+//         return output;
        
 
-}
+// }
 
-console.log(person('shourav','khan','male'))
+// console.log(person('shourav','khan','male'))
+
+
+// let me = {
+//     name : 'shourav',
+//     age :22,
+//     email : 'shourav@gmail.com'
+// }
+
+// function printMySelf(person, callback){
+
+//     console.log('Hi '+  person.name +' '+ 'age is '+' '+ person.age)
+
+//     if(person.age > 18){
+
+//         callback(person.email);
+
+//     }
+// }
+
+//  printMySelf(me, function(email){
+
+//     console.log('Eamil has sent by '+ email );
+
+//  });
+
+
+// function information(data, callback1, callback2){
+
+//     console.log('Trust me: '+ data )
+//     callback1(data);
+//     callback2(data);
+
+// }
+
+// information('There has no peace in my mind, earnival company is responsible for this', function(data){
+
+//     var u = data.toUpperCase();
+
+//     console.log(u);
+// }, function(data){
+
+//     var l = data.toLowerCase();
+
+//     console.log(l);
+
+// });
+
